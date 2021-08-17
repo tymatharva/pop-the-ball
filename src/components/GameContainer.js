ceab109b6712defe5ebdd94763e0050fccbe4935
tymatharva <atharvamohite20.com>
2021-08-17 15:41:02 +0530

    Stars animation half done

--- src/components/GameContainer.js
+++ src/components/GameContainer.js
@@ -3,6 +3,11 @@ import "../styles/GameContainer.scss"
 import Logo from "../assets/Logo.png"
 import Idle from './sprites/Idle'
 import Gun from './sprites/Gun'
+import Bg1 from "../assets/bg1.png"
+import Bg2 from "../assets/bg2.png"
+import Stars2 from "../assets/stars2.png"
+import Planet1 from "../assets/Planet1.png"
+import Planet2 from "../assets/Planet2.png"
 
 const GameContainer = () => {
     return (
@@ -16,11 +21,31 @@ const GameContainer = () => {
             {/* Game Starts */}
             <div className="game-screen container-border">
                 
-                {/* <Idle /> */}
-                <Gun />
+                {/* BG 1 */}
+                <div className="bg-layer-1"><img src={Bg1} alt="" /></div>
+
+                {/* BG 2 */}
+                <div className="bg-layer-2"><img src={Bg2} alt="" /></div>
+
+                {/* Stars */}
+                <div className="stars2 star-animation"><img src={Stars2} alt="" /></div>
+                <div className="stars2 star-animation pos-top-left"><img src={Stars2} alt="" /></div>
+                <div className="stars2 star-animation pos-bot-right"><img src={Stars2} alt="" /></div>
+                <div className="stars2 star-animation pos-bot-left"><img src={Stars2} alt="" /></div>
+
+                {/* Planet 1 */}
+                <div className="planet1"><img src={Planet1} alt="" /></div>
+
+                {/* Planet 2 */}
+                <div className="planet2"><img src={Planet2} alt="" /></div>
+
+                {/* Gradient Layer */}
+                <div className="gradient-layer"></div>
+                <Idle />
+                {/* <Gun /> */}
             </div>
             {/* Game Ends */}
-            {/* <h1>From Game Container</h1> */}
+
         </div>
     )
 }
