8c5235f49e9619bd0d71385bf4a2f47fe32a9779
tymatharva <atharvamohite20.com>
2021-08-18 21:46:41 +0530

    feat: orentation chamged + font added

--- src/App.js
+++ src/App.js
@@ -5,26 +5,53 @@ import GameContainer from './components/GameContainer';
 import ScoreContainer from './components/ScoreContainer';
 import TwoBetScoreContainer from './components/TwoBetScoreContainer';
 import "./styles/common.scss"
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Modal from "react-modal"
+import Land from "./assets/unnamed.png"
 
 function App() {
-  const [twoBet, setTwoBet] = useState(false); 
+  const [twoBet, setTwoBet] = useState(false);
+  const [orient, setOrient] = useState(true); 
   // Modal.setAppElement('#yourAppElement');
+  window.addEventListener("orientationchange", () => {
+    console.log("orientation changed")
+    if(window.matchMedia("(orientation: landscape)").matches){
+      setOrient(true);
+    } else setOrient(false);
+  })
+
+  useEffect(() => {
+      if(window.matchMedia("(orientation: landscape)").matches){
+        setOrient(false);
+      } else setOrient(true);
+  }, []);
   return (
-    <div className="main-container">
-      {/* <Game /> */}
-      {/* Left Sidebar */}
-      <LeftSidebar />
-      {/* Right Sidebar */}
-      <RightSidebar />
-      {/* Game Container */}
-      <GameContainer />
-      {/* Score Container */}
-      {
-        twoBet ? <TwoBetScoreContainer setTwoBet={setTwoBet} /> : <ScoreContainer setTwoBet={setTwoBet} />
+    <>
+    {
+      !orient ? (
+        <>
+        <div className="main-container">
+              {/* <Game /> */}
+              {/* Left Sidebar */}
+              <LeftSidebar />
+              {/* Right Sidebar */}
+              <RightSidebar />
+              {/* Game Container */}
+              <GameContainer />
+              {/* Score Container */}
+              {
+                twoBet ? <TwoBetScoreContainer setTwoBet={setTwoBet} /> : <ScoreContainer setTwoBet={setTwoBet} />
+              }
+        </div>
+            </>
+        ) : (
+          <div className="orientation-img">
+            <img src={Land} alt="" />
+            <div className="orient-text">Please rotate to start</div>
+          </div>
+        )
       }
-    </div>
+      </>
   );
 }
 
